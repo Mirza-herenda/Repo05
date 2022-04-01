0e389c627fd123e49b860d2f5f7a5166cd688b77
Mirza <herendamirza87@gmail.com>
2022-04-01 13:19:05 +0200

    new task:information about me

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function main() 
+{
+
+    var myFirstName="Mirza";
+    var myLastName="Herenda";
+    var myHomeTown="Sarajevo";
+
+    return{myFirstName,myLastName,myHomeTown};
+}
+
+console.log(main());
+module.exports=main;
